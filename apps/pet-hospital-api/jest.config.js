96644b31881c4e64f412898a4adf68a4a333e03d
Netanel Baba <Ntnelbaba@gmail.com>
2021-09-27 10:24:47 +0300

    initial commit

---
+++ apps/pet-hospital-api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'pet-hospital-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/pet-hospital-api',
+};
